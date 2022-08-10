16c653f05ba9a51e2d6cf4d867a16bfe3c968105
pezexd <pedro9031@gmail.com>
2022-08-10 13:50:24 -0400

    2 files modified, 1 added, 2 deleted

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+const nitValue = document.getElementById('nit');
+const nombre = document.getElementById('nombre');
+const fechaNacimiento = document.getElementById('fechaNacimiento');
+const avisoNit = document.getElementById('avisoNit');
+document.getElementById('loadNit').addEventListener('click', loadNit);
+
+function loadNit() {
+  fetch('./nit.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const found = data.find((element) => element.nit == nitValue.value);
+      if (found) {
+        nombre.value = found.nombre;
+        fechaNacimiento.value = found.fecha;
+        avisoNit.innerText = '';
+      } else {
+        avisoNit.innerText = '¡El NIT no se encontró en la base de datos!.';
+        nombre.value = '';
+        fechaNacimiento.value = '';
+      }
+    });
+}
